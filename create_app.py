2d6a7eed9a12f4cfb80128a2ead6f75cf8440271
Corey Oordt <coordt@washingtontimes.com>
2010-06-29 10:57:28 -0400

    Updated the script to be more like the tweaks we added for calloway's script.

--- create_app.py
+++ create_app.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
-import os, random, subprocess
+import os, random, subprocess, sys
+
+HAS_VENV = bool(subprocess.Popen(['which','virtualenv'], stdout=subprocess.PIPE).communicate()[0])
+if not HAS_VENV:
+    print "virtualenv is required to run this script. Please install it with\n  easy_install virtualenv\n\nor\n\n  pip virtualenv"
+    sys.exit(1)
+
+HAS_VENVW = bool(subprocess.Popen(['which','virtualenvwrapper.sh'], stdout=subprocess.PIPE).communicate()[0])
+if not HAS_VENVW:
+    print "virtualenvwrapper is required to run this script. Please install it with\n  easy_install virtualenvwrapper\n\nor\n\n  pip virtualenvwrapper"
+    sys.exit(1)
 
 CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 BLACKLIST = (
@@ -9,7 +19,9 @@ BLACKLIST = (
     'admin/css',
     'admin/img',
     'admin/js',
-    '/.git/'
+    '/.git/',
+    '.svn',
+    '.hg',
 )
 
 def replace(repl, text):
@@ -18,19 +30,16 @@ def replace(repl, text):
         text = text.replace('$$$$%s$$$$' % (key,), value)
     return text
 
-def main():
-    repl = {}
-    an = raw_input('Application name: ')
-    repl['APP_NAME'] = an
+def main(repl, dest, templ_dir):
+    try:
+        os.makedirs(dest)
+    except OSError:
+        pass
     
-    dest_dir = raw_input('Destination directory (currently at %s): ' % (os.getcwd(),))
-    dest = os.path.join(dest_dir, repl['APP_NAME'])
-    os.makedirs(dest)
-    
-    for root, dirs, files in os.walk('./skel/'):
+    for root, dirs, files in os.walk(templ_dir):
         for filename in files:
             source_fn = os.path.join(root, filename)
-            dest_fn = replace(repl, os.path.join(dest, root.replace('./skel/', ''), replace(repl, filename)))
+            dest_fn = replace(repl, os.path.join(dest, root.replace(templ_dir, ''), replace(repl, filename)))
             try:
                 os.makedirs(os.path.dirname(dest_fn))
             except OSError:
@@ -45,14 +54,75 @@ def main():
                 data = replace(repl, data)
             open(dest_fn, 'w').write(data)
             os.chmod(dest_fn, os.stat(source_fn)[0])
-    repl['virtenv'] = raw_input('Virtual environment name (e.g. %s): ' % an)
-    if not repl['virtenv']:
-        repl['virtenv'] = an
     
     print "Making the virtual environment (%s)..." % repl['virtenv']
-    subprocess.call(['source /usr/local/bin/virtualenvwrapper_bashrc;cd %s;mkvirtualenv --no-site-packages %s;easy_install pip' % (dest, repl['virtenv']), ], env=os.environ, executable='/bin/bash', shell=True)
-    subprocess.call(['source /usr/local/bin/virtualenvwrapper_bashrc;cat > $WORKON_HOME/%s/bin/postactivate <<END\n#!/bin/bash/\ncd %s\nEND\nchmod +x $WORKON_HOME/%s/bin/postactivate'%(repl['virtenv'], dest,repl['virtenv'])], env=os.environ, executable='/bin/bash', shell=True)
+    create_env_cmds = [
+        'source virtualenvwrapper.sh', 
+        'cd %s' % dest,
+        'mkvirtualenv --no-site-packages --distribute %s' % repl['virtenv'],
+        'easy_install pip'
+    ]
+    create_pa_cmd = [
+        'source virtualenvwrapper.sh',
+        'cat > $WORKON_HOME/%s/bin/postactivate '\
+        '<<END\n#!/bin/bash/\ncd %s\nEND\n'\
+        'chmod +x $WORKON_HOME/%s/bin/postactivate' % (repl['virtenv'], dest,repl['virtenv'])
+    ]
+    subprocess.call([';'.join(create_env_cmds)], env=os.environ, executable='/bin/bash', shell=True)
+    subprocess.call([';'.join(create_pa_cmd)], env=os.environ, executable='/bin/bash', shell=True)
+    
     print "Now type: workon %s" % repl['virtenv']
 
 if __name__ == '__main__':
-    main()
+    from optparse import OptionParser
+    parser = OptionParser()
+    
+    parser.add_option("-n", "--name", dest="app_name", help="The name of the application.")
+    parser.add_option("-v", "--virtenv", dest="virtenv", help="The name of the virtualenv.")
+    parser.add_option("-d", "--dest", dest="destination", help="Where to put the new application.")
+    parser.add_option("-t", "--template", dest="template", help="The application template to use as a basis for the new application.")
+    (options, args) = parser.parse_args()
+    
+    repl = {
+        'APP_NAME':None,
+    }
+    dest_dir = None
+    templ_dir = None
+    
+    cur_user = os.getlogin()
+    
+    if options.app_name:
+        repl['APP_NAME'] = options.app_name
+    elif len(args) > 0:
+        repl['PROJECT_NAME'] = args[0]
+    
+    while not repl['APP_NAME']:
+        repl['PROJECT_NAME'] = raw_input('Application name: ')
+    
+    repl['SECRET_KEY'] = ''.join([random.choice(CHARS) for i in xrange(50)])
+    
+    if options.destination:
+        dest_dir = options.destination
+    
+    while not dest_dir:
+        dest_dir = raw_input('Destination directory (currently at %s): ' % (os.getcwd(),)) or os.getcwd()
+    dest_dir =  os.path.realpath(os.path.expanduser(dest_dir))
+    dest = os.path.join(dest_dir, repl['PROJECT_NAME'])
+
+    if options.template:
+        templ_dir = options.template
+
+    default = os.path.abspath(os.path.join(os.path.dirname(__file__), 'skel_tmpl'))
+    while not templ_dir:
+        templ_dir = raw_input('Application template directory [%s]: ' % default) or default
+    templ_dir = os.path.realpath(os.path.expanduser(templ_dir))
+    if templ_dir[-1] != '/':
+        templ_dir = templ_dir + "/"
+    if options.virtenv:
+        repl['virtenv'] = options.virtenv
+
+    repl['virtenv'] = None
+    while not repl['virtenv']:
+        repl['virtenv'] = raw_input('Virtual environment name (e.g. %s): ' % repl['APP_NAME']) or repl['APP_NAME']
+
+    main(repl, dest, templ_dir)
