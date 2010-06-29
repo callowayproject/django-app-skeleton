e8cb056dee48574b3a9824c44089aa3189f57735
Corey Oordt <coordt@washingtontimes.com>
2010-06-29 11:08:18 -0400

    Added a basic setup script for packaging

---
+++ skel/setup.py
@@ -0,0 +1,26 @@
+import os, sys
+from setuptools import setup, find_packages
+
+def read_file(filename):
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except:
+        return ''
+
+setup(
+    name = "$$$$APP_NAME$$$$",
+    version = __import__('$$$$APP_NAME$$$$').get_version().replace(' ', '-'),
+    url = '',
+    author = '$$$$AUTHOR$$$$',
+    author_email = '',
+    description = '',
+    long_description = read_file('README'),
+    packages = find_packages(),
+    include_package_data = True,
+    classifiers = [
+    ],
+)
+
+
