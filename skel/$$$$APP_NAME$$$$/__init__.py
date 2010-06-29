731146106cfd6bb123637d7c6c8a4904c8863033
Corey Oordt <coordt@washingtontimes.com>
2010-06-29 11:03:45 -0400

    Added a version string for version tracking

---
+++ skel/$$$$APP_NAME$$$$/__init__.py
@@ -0,0 +1,18 @@
+__version_info__ = {
+    'major': 0,
+    'minor': 1,
+    'micro': 0,
+    'releaselevel': 'final',
+    'serial': 1
+}
+
+def get_version():
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final':
+        vers.append('%(releaselevel)s%(serial)i' % __version_info__)
+    return ''.join(vers)
+
+__version__ = get_version()
