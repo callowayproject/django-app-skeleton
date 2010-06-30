c71c91f2a20a2c0e3ba8b25ed4ba287bd007b35c
Corey Oordt <coordt@washingtontimes.com>
2010-06-30 13:06:32 -0400

    Added a blank requirements file and added it to the setup

--- skel/setup.py
+++ skel/setup.py
@@ -19,6 +19,7 @@ setup(
     long_description = read_file('README'),
     packages = find_packages(),
     include_package_data = True,
+    install_requires=read_file('requirements.txt'),
     classifiers = [
     ],
 )
