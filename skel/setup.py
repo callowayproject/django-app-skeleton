512bae01790f728f414fc411f5290fa938d9125a
Corey Oordt <coordt@washingtontimes.com>
2010-09-30 11:27:35 -0400

    Updated the setup.py to include better error handling and automatically get the short description from the docstring on the __index__.py file

--- skel/setup.py
+++ skel/setup.py
@@ -1,13 +1,16 @@
-import os, sys
+import os
 from setuptools import setup, find_packages
 
 def read_file(filename):
+    """Read a file into a string"""
     path = os.path.abspath(os.path.dirname(__file__))
     filepath = os.path.join(path, filename)
     try:
         return open(filepath).read()
-    except:
+    except IOError:
         return ''
+
+DESC = " ".join(__import__('alphafilter').__doc__.splitlines()).strip()
 
 setup(
     name = "$$$$APP_NAME$$$$",
@@ -15,9 +18,10 @@ setup(
     url = '',
     author = '$$$$AUTHOR$$$$',
     author_email = '',
-    description = '',
+    description = DESC,
     long_description = read_file('README'),
     packages = find_packages(),
+    license = 'Apache 2.0',
     include_package_data = True,
     install_requires=read_file('requirements.txt'),
     classifiers = [
