c31b87096c01df90603e982b697d6196d84eed49
Corey Oordt <coordt@washingtontimes.com>
2010-11-04 09:11:46 -0400

    Replacing APP_NAME to PKG_NAME in appropriate places

--- skel/setup.py
+++ skel/setup.py
@@ -10,20 +10,22 @@ def read_file(filename):
     except IOError:
         return ''
 
-DESC = " ".join(__import__('alphafilter').__doc__.splitlines()).strip()
+# Use the docstring of the __init__ file to be the description
+DESC = " ".join(__import__('$$$$PKG_NAME$$$$').__doc__.splitlines()).strip()
 
 setup(
     name = "$$$$APP_NAME$$$$",
-    version = __import__('$$$$APP_NAME$$$$').get_version().replace(' ', '-'),
+    version = __import__('$$$$PKG_NAME$$$$').get_version().replace(' ', '-'),
     url = '',
     author = '$$$$AUTHOR$$$$',
     author_email = '',
     description = DESC,
     long_description = read_file('README'),
     packages = find_packages(),
-    license = 'Apache 2.0',
     include_package_data = True,
     install_requires=read_file('requirements.txt'),
     classifiers = [
+        'License :: OSI Approved :: Apache Software License',
+        'Framework :: Django',
     ],
 )
