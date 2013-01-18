58e5b2a1a07cfc4e9722d38504800a79bd369473
Jose Soares <josefsoares@gmail.com>
2013-01-18 10:41:03 -0500

    [1.0] Cleaned up the create_app script
    - Nothing new yet, just made it easier to read

--- create_app.py
+++ create_app.py
@@ -2,17 +2,26 @@
 
 import os
 import random
-import subprocess
 import sys
+import subprocess
 
-HAS_VENV = bool(subprocess.Popen(['which', 'virtualenv'], stdout=subprocess.PIPE).communicate()[0])
+HAS_VENV = bool(subprocess.Popen(
+    ['which', 'virtualenv'],
+    stdout=subprocess.PIPE).communicate()[0])
+
 if not HAS_VENV:
-    print "virtualenv is required to run this script. Please install it with\n  easy_install virtualenv\n\nor\n\n  pip virtualenv"
+    print "virtualenv is required to run this script. Please install it " \
+          "with\n  easy_install virtualenv\n\nor\n\n  pip virtualenv"
     sys.exit(1)
 
-HAS_VENVW = bool(subprocess.Popen(['which', 'virtualenvwrapper.sh'], stdout=subprocess.PIPE).communicate()[0])
+HAS_VENVW = bool(subprocess.Popen(
+    ['which', 'virtualenvwrapper.sh'],
+    stdout=subprocess.PIPE).communicate()[0])
+
 if not HAS_VENVW:
-    print "virtualenvwrapper is required to run this script. Please install it with\n  easy_install virtualenvwrapper\n\nor\n\n  pip virtualenvwrapper"
+    print "virtualenvwrapper is required to run this script. Please install " \
+          "it with\n  easy_install virtualenvwrapper\n\nor\n\n  pip " \
+          "virtualenvwrapper"
     sys.exit(1)
 
 CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
@@ -44,7 +53,8 @@ def main(repl, dest, templ_dir):
     for root, dirs, files in os.walk(templ_dir):
         for filename in files:
             source_fn = os.path.join(root, filename)
-            dest_fn = replace(repl, os.path.join(dest, root.replace(templ_dir, ''), replace(repl, filename)))
+            dest_fn = replace(repl, os.path.join(
+                dest, root.replace(templ_dir, ''), replace(repl, filename)))
             try:
                 os.makedirs(os.path.dirname(dest_fn))
             except OSError:
@@ -60,7 +70,7 @@ def main(repl, dest, templ_dir):
             open(dest_fn, 'w').write(data)
             os.chmod(dest_fn, os.stat(source_fn)[0])
 
-    print "Making the virtual environment (%s)..." % repl['VIRTENV']
+    print 'Making the virtual environment (%s)...' % repl['VIRTENV']
     create_env_cmds = [
         'source virtualenvwrapper.sh',
         'cd %s' % dest,
@@ -69,26 +79,39 @@ def main(repl, dest, templ_dir):
     ]
     create_pa_cmd = [
         'source virtualenvwrapper.sh',
-        'cat > $WORKON_HOME/%s/bin/postactivate '\
+        'cat > $WORKON_HOME/%s/bin/postactivate'\
         '<<END\n#!/bin/bash/\ncd %s\nEND\n'\
-        'chmod +x $WORKON_HOME/%s/bin/postactivate' % (repl['VIRTENV'], dest, repl['VIRTENV'])
+        'chmod +x $WORKON_HOME/%s/bin/postactivate' % (repl['VIRTENV'],
+                                                       dest,
+                                                       repl['VIRTENV'])
     ]
-    subprocess.call([';'.join(create_env_cmds)], env=os.environ, executable='/bin/bash', shell=True)
-    subprocess.call([';'.join(create_pa_cmd)], env=os.environ, executable='/bin/bash', shell=True)
+    subprocess.call([';'.join(create_env_cmds)], env=os.environ,
+                    executable='/bin/bash', shell=True)
+    subprocess.call([';'.join(create_pa_cmd)], env=os.environ,
+                    executable='/bin/bash', shell=True)
 
-    print "Now type: workon %s" % repl['VIRTENV']
+    print 'Now type: workon %s' % repl['VIRTENV']
 
 if __name__ == '__main__':
     from optparse import OptionParser
     parser = OptionParser()
-    parser.add_option("-a", "--author", dest="author", help="The name of the author.")
-    parser.add_option("-e", "--author-email", dest="author_email", help="The email of the author.")
-    parser.add_option("-u", "--url", dest="url", help="The URL of the project page.")
-    parser.add_option("-n", "--name", dest="app_name", help="The name of the application, like 'django-coolapp'.")
-    parser.add_option("-p", "--package", dest="pkg_name", help="The name of the installed package, like 'coolapp'.")
-    parser.add_option("-v", "--VIRTENV", dest="VIRTENV", help="The name of the virtualenv.")
-    parser.add_option("-d", "--dest", dest="destination", help="Where to put the new application. Relative paths are recognized.")
-    parser.add_option("-t", "--template", dest="template", help="The application template to use as a basis for the new application.")
+    parser.add_option('-a', '--author', dest='author',
+                      help='The name of the author.')
+    parser.add_option('-e', '--author-email', dest='author_email',
+                      help='The email of the author.')
+    parser.add_option('-u', '--url', dest='url',
+                      help='The URL of the project page.')
+    parser.add_option('-n', '--name', dest='app_name',
+                      help='The name of the application, i.e. django-myapp')
+    parser.add_option('-p', '--package', dest='pkg_name',
+                      help='The name of the installed package, i.e. myapp')
+    parser.add_option('-v', '--VIRTENV', dest='VIRTENV',
+                      help='The name of the virtualenv.')
+    parser.add_option('-d', '--dest', dest='destination',
+                      help='Where to put the new application.')
+    parser.add_option('-t', '--template', dest='template',
+                      help='''The application template to use as a basis for
+                              the new application.''')
     (options, args) = parser.parse_args()
 
     repl = {
@@ -114,13 +137,16 @@ if __name__ == '__main__':
     if options.pkg_name:
         repl['PKG_NAME'] = options.pkg_name
     while not repl['PKG_NAME']:
-        default_name = repl['APP_NAME'].replace('django-', '').replace('-', '_')
-        repl['PKG_NAME'] = raw_input('Package Name [%s]:' % default_name) or default_name
+        default_name = repl['APP_NAME'].replace(
+            'django-', '').replace('-', '_')
+        repl['PKG_NAME'] = raw_input(
+            'Package Name [%s]:' % default_name) or default_name
 
     if options.author:
         repl['AUTHOR'] = options.author
     while not repl['AUTHOR']:
-        repl['AUTHOR'] = raw_input('Author [%s]:' % cur_user) or cur_user
+        repl['AUTHOR'] = raw_input(
+            'Author [%s]:' % cur_user) or cur_user
 
     if options.author:
         repl['AUTHOR_EMAIL'] = options.author_email
@@ -138,7 +164,8 @@ if __name__ == '__main__':
         dest_dir = options.destination
 
     while not dest_dir:
-        dest_dir = raw_input('Destination directory [%s]: ' % (os.getcwd(),)) or os.getcwd()
+        dest_dir = raw_input(
+            'Destination directory [%s]: ' % (os.getcwd(),)) or os.getcwd()
     dest_dir = os.path.realpath(os.path.expanduser(dest_dir))
     dest = os.path.join(dest_dir, repl['APP_NAME'])
 
@@ -147,7 +174,8 @@ if __name__ == '__main__':
 
     default = os.path.abspath(os.path.join(os.path.dirname(__file__), 'skel'))
     while not templ_dir:
-        templ_dir = raw_input('Application template directory [%s]: ' % default) or default
+        templ_dir = raw_input(
+            'Application template directory [%s]: ' % default) or default
     templ_dir = os.path.realpath(os.path.expanduser(templ_dir))
     if templ_dir[-1] != '/':
         templ_dir = templ_dir + "/"
@@ -157,6 +185,8 @@ if __name__ == '__main__':
     else:
         repl['VIRTENV'] = None
     while not repl['VIRTENV']:
-        repl['VIRTENV'] = raw_input('Virtual environment name [%s]: ' % repl['PKG_NAME']) or repl['PKG_NAME']
+        repl['VIRTENV'] = raw_input(
+            'Virtual environment name [%s]: ' % (
+                repl['PKG_NAME']) or repl['PKG_NAME'])
 
     main(repl, dest, templ_dir)
