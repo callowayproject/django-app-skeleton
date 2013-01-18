e94d8b34569ea7d6626b33bc8f0c9220c248a4ad
Jose Soares <josefsoares@gmail.com>
2013-01-18 11:28:43 -0500

    [1.0] Added some extra default code to variouse files
    - models has some more imports
    - template tags has the Library imported and assigned to register
    - removed the `import *` from and urls
    - views has some more `common` imports

--- skel/$$$$PKG_NAME$$$$/urls.py
+++ skel/$$$$PKG_NAME$$$$/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import patterns, url
+
 
 urlpatterns = patterns('',
     url(r'^$', 'views.index', name='index'),
--- skel/$$$$PKG_NAME$$$$/views.py
+++ skel/$$$$PKG_NAME$$$$/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render_to_response
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponseRedirect, HttpResponseForbidden, Http404
-from django.template import RequestContext
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.cache import never_cache, cache_page
+from django.views.decorators.http import require_http_methods
