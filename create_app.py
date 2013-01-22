683c2784cb78d801aeb62683cde039918b1235cf
Jose Soares <jose@linux.com>
2013-01-22 07:44:32 -0500

    [1.0] Re-worked the create_app script
    - most changes were to enhance readability
    - virtual env is optional now
    - config file usage updates

--- create_app.py
+++ create_app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import ConfigParser
 import os
@@ -6,27 +7,8 @@ import random
 import sys
 import subprocess
 
-HAS_VENV = bool(subprocess.Popen(
-    ['which', 'virtualenv'],
-    stdout=subprocess.PIPE).communicate()[0])
-
-if not HAS_VENV:
-    print "virtualenv is required to run this script. Please install it " \
-          "with\n  easy_install virtualenv\n\nor\n\n  pip virtualenv"
-    sys.exit(1)
-
-HAS_VENVW = bool(subprocess.Popen(
-    ['which', 'virtualenvwrapper.sh'],
-    stdout=subprocess.PIPE).communicate()[0])
 
 CONFIG_FILE = os.path.expanduser('~/.djas')
-
-if not HAS_VENVW:
-    print "virtualenvwrapper is required to run this script. Please install " \
-          "it with\n  easy_install virtualenvwrapper\n\nor\n\n  pip " \
-          "virtualenvwrapper"
-    sys.exit(1)
-
 CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 BLACKLIST = (
     'jquery',
@@ -38,6 +20,13 @@ BLACKLIST = (
     '.svn',
     '.hg',
 )
+
+
+def write_config(config):
+    """Writes the config to a file"""
+    with open(CONFIG_FILE, 'wb') as conf:
+        config.write(conf)
+
 
 def get_config_value(config, sec, opt, default):
     """Get a config value.
@@ -52,9 +41,24 @@ def get_config_value(config, sec, opt, default):
         if not config.has_section('main'):
             config.add_section('main')
         config.set(sec, opt, default)
-        with open(CONFIG_FILE, 'wb') as cf:
-            config.write(cf)
+        write_config(config)
         return default
+
+
+def set_config_value(sec, opt, value):
+    """Set a config value.
+
+    Only sets the value if the value is not founc of empty.
+
+    """
+    config = ConfigParser.RawConfigParser()
+    config.read(CONFIG_FILE)
+
+    if not config.has_option(sec, opt):
+        config.set(sec, opt, value)
+    elif config.get(sec, opt) == '':
+        config.set(sec, opt, value)
+    write_config(config)
 
 
 def get_config():
@@ -63,9 +67,10 @@ def get_config():
     defaults = (
         ('author', 'PKG_AUTHOR', ''),
         ('author_email', 'PKG_AUTHOR_EMAIL', ''),
-        ('destination_dir', 'DESTINATION_DIR',  os.getcwd()),
-        ('skel_dir', 'SKEL_DIR', os.path.abspath(
+        ('destintation_dir', 'DEST_DIR',  os.getcwd()),
+        ('template_dir', 'TMPL_DIR', os.path.abspath(
             os.path.join(os.path.dirname(__file__), 'skel'))),
+        ('use_venv', 'USE_VENV', 'n'),
     )
 
     config = ConfigParser.RawConfigParser()
@@ -75,26 +80,82 @@ def get_config():
         for default in defaults:
             key, var, value = default
             config.set('main', key, value)
-
-        # Write out the default config
-        with open(CONFIG_FILE, 'wb') as cf:
-            config.write(cf)
+        write_config(config)
 
     return dict([
         (default[1], get_config_value(
             config, 'main', default[0], default[2])) for default in defaults])
 
 
-def replace(repl, text):
-    text = text.replace('/app_name', '/{0}'.format(repl['APP_NAME']))
+def ensure_venv():
+    """Ensure virtualenv and virtuelenv wrapper is installed"""
+    has_venv = bool(subprocess.Popen(
+        ['which', 'virtualenv'],
+        stdout=subprocess.PIPE).communicate()[0])
+
+    if not has_venv:
+        print 'virtualenv is required to run this script. Please install it ' \
+              'with\n  easy_install virtualenv\n\nor\n\n  pip virtualenv'
+        sys.exit(1)
+
+    has_venv_wrapper = bool(subprocess.Popen(
+        ['which', 'virtualenvwrapper.sh'],
+        stdout=subprocess.PIPE).communicate()[0])
+
+    if not has_venv_wrapper:
+        print 'virtualenvwrapper is required to run this script. Please' \
+              'install it with\n  easy_install virtualenvwrapper\n\nor\n\n' \
+              'pip virtualenvwrapper'
+        sys.exit(1)
+
+
+def mk_virtual_env(name, dest):
+    """Creates a virtualenv using virtualenv wrapper"""
+    print 'Making the virtual environment (%s)...' % name
+    create_env_cmds = [
+        'source virtualenvwrapper.sh',
+        'cd %s' % dest,
+        'mkvirtualenv --no-site-packages --distribute %s' % name,
+        'easy_install pip'
+    ]
+    create_pa_cmd = [
+        'source virtualenvwrapper.sh',
+        'cat > $WORKON_HOME/%s/bin/postactivate'\
+        '<<END\n#!/bin/bash/\ncd %s\nEND\n'\
+        'chmod +x $WORKON_HOME/%s/bin/postactivate' % (name,
+                                                       dest,
+                                                       name)
+    ]
+    subprocess.call([';'.join(create_env_cmds)], env=os.environ,
+                    executable='/bin/bash', shell=True)
+    subprocess.call([';'.join(create_pa_cmd)], env=os.environ,
+                    executable='/bin/bash', shell=True)
+
+    print 'Virtualenv created, type workon %s' % name
+
+
+def replace(opts, text):
+    """Replace certain strings will the supplied text
+
+    `opts` is a dictionary of variables that will be replaced. Similar to
+    django, it will look for {{..}} and replace it with the variable value
+
+    Since we want to maintance compatibility with django's `startapp` command
+    we need to also replaced `app_name` folders with the supplied value.
+
+    """
+    text = text.replace('/app_name', '/{0}'.format(opts['APP_NAME']))
     text = text.replace('/gitignore', '/.gitignore')
 
-    for key, value in repl.iteritems():
+    for key, value in opts.iteritems():
+        if not value:
+            continue
         text = text.replace('{{%s}}' % (key.lower(),), value)
     return text
 
 
-def main(repl, dest, templ_dir):
+def mk_pkg(opts, dest, templ_dir):
+    """Creates the package file/folder structure"""
     try:
         os.makedirs(dest)
     except OSError:
@@ -103,12 +164,15 @@ def main(repl, dest, templ_dir):
     for root, dirs, files in os.walk(templ_dir):
         for filename in files:
             source_fn = os.path.join(root, filename)
-            dest_fn = replace(repl, os.path.join(
-                dest, root.replace(templ_dir, ''), replace(repl, filename)))
+
+            dest_fn = replace(opts, os.path.join(
+                dest, root.replace(templ_dir, ''), replace(opts, filename)))
+
             try:
                 os.makedirs(os.path.dirname(dest_fn))
             except OSError:
                 pass
+
             print 'Copying %s to %s' % (source_fn, dest_fn)
             should_replace = True
             for bl_item in BLACKLIST:
@@ -116,31 +180,80 @@ def main(repl, dest, templ_dir):
                     should_replace = False
             data = open(source_fn, 'r').read()
             if should_replace:
-                data = replace(repl, data)
+                data = replace(opts, data)
             open(dest_fn, 'w').write(data)
             os.chmod(dest_fn, os.stat(source_fn)[0])
+    print 'Package created'
 
-    print 'Making the virtual environment (%s)...' % repl['VENV']
-    create_env_cmds = [
-        'source virtualenvwrapper.sh',
-        'cd %s' % dest,
-        'mkvirtualenv --no-site-packages --distribute %s' % repl['VENV'],
-        'easy_install pip'
-    ]
-    create_pa_cmd = [
-        'source virtualenvwrapper.sh',
-        'cat > $WORKON_HOME/%s/bin/postactivate'\
-        '<<END\n#!/bin/bash/\ncd %s\nEND\n'\
-        'chmod +x $WORKON_HOME/%s/bin/postactivate' % (repl['VENV'],
-                                                       dest,
-                                                       repl['VENV'])
-    ]
-    subprocess.call([';'.join(create_env_cmds)], env=os.environ,
-                    executable='/bin/bash', shell=True)
-    subprocess.call([';'.join(create_pa_cmd)], env=os.environ,
-                    executable='/bin/bash', shell=True)
 
-    print 'Now type: workon %s' % repl['VENV']
+def main(options):
+    config = get_config()
+    cur_user = os.getlogin()
+    # Default options
+    opts = {
+        'APP_NAME': None,
+        'PKG_NAME': None,
+        'PKG_AUTHOR': None,
+        'PKG_AUTHOR_EMAIL': None,
+        'PKG_URL': None,
+        'VENV': None,
+        'SECRET_KEY': ''.join([random.choice(CHARS) for i in xrange(50)]),
+        'DEST_DIR': None,
+        'TMPL_DIR': None,
+        'USE_VENV': None
+    }
+
+    # Update the default options wiht the config values
+    opts.update(config)
+
+    def prompt(attr, text, default=None):
+        """Prompt the user for certain values"""
+        if hasattr(options, attr):
+            if getattr(options, attr):
+                return getattr(options, attr)
+
+        default_text = default and ' [%s]: ' % default or ': '
+        new_val = None
+        while not new_val:
+            new_val = raw_input(text + default_text) or default
+        return new_val
+
+    # Package/App Information
+    opts['PKG_NAME'] = prompt('pkg_name', 'Package Name')
+    opts['APP_NAME'] = prompt(
+        'app_name', 'App Name', opts['PKG_NAME'].replace('django-', ''))
+    opts['PKG_URL'] = prompt('pkg_url', 'Project URL')
+
+    # Author Information
+    opts['PKG_AUTHOR'] = prompt(
+        'pkg_author', 'Author Name', opts['PKG_AUTHOR'] or cur_user)
+    opts['PKG_AUTHOR_EMAIL'] = prompt(
+        'pkg_author_email', 'Author Email', opts['PKG_AUTHOR_EMAIL'])
+
+    set_config_value('main', 'author', opts['PKG_AUTHOR'])
+    set_config_value('main', 'author_email', opts['PKG_AUTHOR_EMAIL'])
+
+    # Destination and template directories
+    opts['DEST_DIR'] = prompt(
+        'destination', 'Destination DIR', opts['DEST_DIR'])
+    opts['DEST_DIR'] = os.path.join(opts['DEST_DIR'], opts['PKG_NAME'])
+
+    opts['TMPL_DIR'] = prompt('template', 'Template DIR', opts['TMPL_DIR'])
+
+    tmpl_dir = os.path.realpath(os.path.expanduser(opts['TMPL_DIR']))
+    if tmpl_dir[-1] != '/':
+        tmpl_dir = tmpl_dir + "/"
+    opts['TMPL_DIR'] = tmpl_dir
+
+    # Copy the template and replace the proper values
+    mk_pkg(opts, opts['DEST_DIR'], opts['TMPL_DIR'])
+
+    # Virtualenv
+    opts['USE_VENV'] = prompt('use_venv', 'Use Virtualenv', 'n')
+    if opts['USE_VENV'] in ['y', 'yes', '1']:
+        opts['VENV'] = prompt('venv', 'Virtualenv Name', opts['PKG_NAME'])
+        mk_virtual_env(opts['VENV'], opts['DEST_DIR'])
+
 
 if __name__ == '__main__':
     from optparse import OptionParser
@@ -164,96 +277,4 @@ if __name__ == '__main__':
                            'the new application.')
     (options, args) = parser.parse_args()
 
-    # Get the config values
-    config = get_config()
-
-    repl = {
-        'APP_NAME': None,
-        'PKG_NAME': None,
-        'PKG_AUTHOR': None,
-        'PKG_AUTHOR_EMAIL': None,
-        'PKG_URL': None,
-        'VENV': None,
-        'SECRET_KEY': ''.join([random.choice(CHARS) for i in xrange(50)]),
-        'DESTINATION_DIR': None,
-        'SKEL_DIR': None
-    }
-
-    repl.update(config)
-
-    dest_dir = repl['DESTINATION_DIR']
-    templ_dir = repl['SKEL_DIR']
-
-    cur_user = os.getlogin()
-
-    # Package name i.e. django-package
-    if options.pkg_name:
-        repl['PKG_NAME'] = options.pkg_name
-    while not repl['PKG_NAME']:
-        repl['PKG_NAME'] = raw_input('Package Name: ')
-
-    # App name i.e. package
-    if options.app_name:
-        repl['APP_NAME'] = options.app_name
-
-    pkg_name = repl['PKG_NAME']
-    app_name = repl['APP_NAME'] or pkg_name.replace('django-', '')
-    while not repl['APP_NAME']:
-        repl['APP_NAME'] = raw_input(
-            'App name [{0}]: '.format(app_name)) or app_name
-
-    # Author name
-    if options.pkg_author:
-        repl['PKG_AUTHOR'] = options.pkg_author
-    while not repl['PKG_AUTHOR']:
-        repl['PKG_AUTHOR'] = raw_input(
-            'Author [%s]:' % cur_user) or cur_user
-
-    if options.pkg_author_email:
-        repl['PKG_AUTHOR_EMAIL'] = options.pkg_author_email
-    while not repl['PKG_AUTHOR_EMAIL']:
-        repl['PKG_AUTHOR_EMAIL'] = raw_input('Author\'s Email: ')
-
-    # Package Url
-    if options.pkg_url:
-        repl['PKG_URL'] = options.pkg_url
-    while not repl['PKG_URL']:
-        repl['PKG_URL'] = raw_input('Project Page URL: ')
-
-    if options.destination:
-        dest_dir = options.destination
-
-    new_dest_dir = None
-    # Destination directory
-    while not new_dest_dir:
-        new_dest_dir = raw_input(
-            'Destination directory [%s]: ' % (dest_dir,)) or dest_dir
-
-    dest_dir = new_dest_dir
-    dest_dir = os.path.realpath(os.path.expanduser(dest_dir))
-    dest = os.path.join(dest_dir, repl['PKG_NAME'])
-
-    if options.template:
-        templ_dir = options.template
-
-    new_skel_dir = None
-    while not new_skel_dir:
-        new_skel_dir = raw_input(
-            'Application template directory [%s]: ' % skel_dir) or skel_dir
-
-    skel_dir = new_skel_dir
-    skel_dir = os.path.realpath(os.path.expanduser(skel_dir))
-
-    if skel_dir[-1] != '/':
-        skel_dir = skel_dir + "/"
-
-    if options.venv:
-        repl['VENV'] = options.venv
-    else:
-        repl['VENV'] = None
-    while not repl['VENV']:
-        repl['VENV'] = raw_input(
-            'Virtual environment name [%s]: ' % (
-                repl['PKG_NAME'])) or repl['PKG_NAME']
-
-    main(repl, dest, skel_dir)
+    sys.exit(main(options))
