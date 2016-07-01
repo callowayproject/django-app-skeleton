6665c61d12dcd30a43677224d3eb2d14c22ed500
Corey Oordt <coreyoordt@gmail.com>
2016-07-01 07:39:23 -0500

    Add unicode literals into all the python files

--- skel/app_name/models.py
+++ skel/app_name/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
-from django.conf import settings as site_settings
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
-from {{app_name}} import settings
+# from .settings import #
--- skel/app_name/views.py
+++ skel/app_name/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponseRedirect, HttpResponseForbidden, Http404
